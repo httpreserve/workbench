98b8d050da56226a29432856283121f0fd1302dc
ross-spencer <allalongthewatchtower2001@gmail.com>
2017-03-27 18:27:11 +1300

    [feature] A small bit of JSON. Work in progress.

--- httpreserveapp.go
+++ httpreserveapp.go
@@ -16,6 +16,9 @@ var (
 	link       string
 	linklabel  string
 	remote     bool
+
+	//testing
+	test 			bool
 )
 
 func init() {
@@ -27,6 +30,9 @@ func init() {
 	flag.StringVar(&link, "link", "", "Seek the status of a single weblink.")
 	flag.StringVar(&linklabel, "linklabel", "", "Annotate response with filename, or label.")
 	flag.BoolVar(&remote, "remote", false, "Send requests to remote connection.")
+
+	//testing flags for testing whatever we're working on at present
+	flag.BoolVar(&test, "test", false, "use test function while developing functionality.")
 }
 
 func demosetup() {
@@ -55,18 +61,64 @@ func getRemoteLink() {
 	}
 }
 
-func getLocalLink() {
+func libLink(link string, linklabel string) string {
 	ls, err := httpreserve.GenerateLinkStats(link, linklabel)
 	if err != nil {
 		log.Println("Error retrieving linkstat:", err)
-		return
+		return ""
 	}
 	js := httpreserve.MakeLinkStatsJSON(ls)
+	return js
+}
+
+func getLocalLink() {
+	js := libLink(link, linklabel)
 	fmt.Fprintln(os.Stderr, "Using httpreserve libs to retrieve data.")
 	fmt.Fprintf(os.Stdout, "%s", js)
 }
 
+func channelLocalLink(link string, filename string, ch chan string) {
+	ch <- libLink(link, filename)
+}
+
+var linkmap = map[string]string{
+	"http://www.bbc.co.uk/news": "bbc news",
+	"http://www.bbc.co.uk/": "bbc home",	
+	"http://www.bbc.co.uk/radio": "bbc radio",	
+	"http://www.nationalarchives.gov.uk/": "tna",
+}
+
+func testfunction() {
+   ch := make(chan string)
+   for l, f := range linkmap {
+		go channelLocalLink(l, f, ch)	
+   }
+
+
+   fmt.Println("{")
+   fmt.Println("\"title\": \"httpreserve client example\",")
+   fmt.Println("\"data\": [")   
+
+   var count int
+   for range linkmap {
+   	count+=1
+   	ce := <- ch
+   	fmt.Print(ce)
+   	if count < len(linkmap) {
+   		fmt.Println(",")
+   	}
+   }
+
+   fmt.Println("]\n}") 
+}
+
 func programrunner() {
+
+	if test {
+		testfunction()
+		return
+	}
+
 	if demo {
 		//setup our demo server to communicate with
 		demosetup()
@@ -92,6 +144,9 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Usage:  httpreserve-app [Optional -demo] [Optional -demoport] [Optional -method]")
 		fmt.Fprintln(os.Stderr, "                        [Optional -link] [Optional -linklabel] [Optional -remote]")
 		fmt.Fprintln(os.Stderr, "                        [Optional -version -v]")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "        -test")		
+		fmt.Fprintln(os.Stderr, "")		
 		fmt.Fprintln(os.Stderr, "Output: [SERVER] 127.0.0.1:2040, [SERVER] 127.0.0.1:{port}")
 		fmt.Fprintf(os.Stderr, "Output: [JSON] '%s ...'\n", "{ \"httpreserveanalysis\": \"x,y,z\" }")
 		fmt.Fprintf(os.Stderr, "Output: [VERSION] '%s ...'\n", httpreserve.VersionText())
